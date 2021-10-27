d34b8aeca6bdff2811c56bce6328544eec809de3
Alexander <81567588+sashacozub@users.noreply.github.com>
2021-10-27 15:47:23 +0200

    Update openweather icon link

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -19,7 +19,7 @@ const Display = ({ currentWeather, isMetric, onClick, isLoading, isError }) => {
     <>
       <div className='main-weather-info-box'>
         <img
-          src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+          src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
           alt={`${description} icon`}
         />
 
